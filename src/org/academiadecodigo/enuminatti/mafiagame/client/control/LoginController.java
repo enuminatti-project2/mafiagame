1fd187417a91f517dd5ea7ae081caf7bce8b9c47
Rodrigo Sanches <rodrigosanches666@gmail.com>
2017-11-08 18:55:22 +0000

    Login UI and his respectiv controler

---
+++ src/org/academiadecodigo/enuminatti/mafiagame/client/control/LoginController.java
@@ -0,0 +1,28 @@
+package org.academiadecodigo.enuminatti.mafiagame.client.control;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+
+/**
+ * Created by codecadet on 08/11/17.
+ */
+public class LoginController {
+
+    @FXML
+    private TextField ipAddressTextField;
+
+    @FXML
+    private TextField usernameTextField;
+
+    @FXML
+    private TextArea serverMessageArea;
+
+    @FXML
+    void connectoServer(ActionEvent event) {
+
+    }
+
+}
