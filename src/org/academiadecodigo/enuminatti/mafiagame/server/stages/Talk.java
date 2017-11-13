c15d18349c06daf9ddade9f618d68b7898624a84
Ricardo Constantino <wiiaboo@gmail.com>
2017-11-13 20:53:27 +0000

    Stages: implement Talk timer

--- src/org/academiadecodigo/enuminatti/mafiagame/server/stages/Talk.java
+++ src/org/academiadecodigo/enuminatti/mafiagame/server/stages/Talk.java
@@ -1,8 +1,14 @@
 package org.academiadecodigo.enuminatti.mafiagame.server.stages;
 
 import org.academiadecodigo.enuminatti.mafiagame.server.game.GameMaster;
+import org.academiadecodigo.enuminatti.mafiagame.server.util.Broadcaster;
+import org.academiadecodigo.enuminatti.mafiagame.utils.EncodeDecode;
 
 import java.util.Set;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 /**
  * MIT License
@@ -10,14 +16,57 @@ import java.util.Set;
  */
 
 public class Talk implements Stage {
+    private final int SECONDS_TO_TALK = 5 * 60; // 5 minutes
+
+    private GameMaster gameMaster;
+
+    private Set<String> talkers;
+
+    private ScheduledExecutorService talkRunner;
+    private ScheduledFuture<?> talkTimer;
 
     public Talk(GameMaster gameMaster) {
-
+        this.gameMaster = gameMaster;
     }
 
 
     @Override
-    public void startStage(Set<String> activePlayersOnStage, Set<String> possibleTargets) {
+    public void runStage(Set<String> activePlayersOnStage, Set<String> possibleTargets) {
+        this.talkers = activePlayersOnStage;
 
+        startTimer();
+    }
+
+    private void startTimer() {
+        if (talkRunner == null) {
+            talkRunner = Executors.newSingleThreadScheduledExecutor();
+        }
+
+        if (talkTimer != null) {
+            talkTimer.cancel(true);
+        }
+
+        talkTimer = talkRunner.schedule(this::endTimer, SECONDS_TO_TALK, TimeUnit.SECONDS);
+
+        Broadcaster.broadcastToPlayers(gameMaster.getListOfPlayers(), talkers,
+                EncodeDecode.TIMER, Integer.toString(SECONDS_TO_TALK));
+    }
+
+    private void endTimer() {
+
+        talkTimer.cancel(true);
+        goNext();
+
+    }
+
+    private void goNext() {
+        gameMaster.changeStage(Stages.VOTE);
+    }
+
+    @Override
+    public void cleanup() {
+        if (talkRunner != null) {
+            talkRunner.shutdown();
+        }
     }
 }
